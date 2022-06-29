3dd6aeadab9043ccd82b78870fef161607a3768a
fhildeb <felix@lukso.io>
2022-06-29 22:45:00 +0200

    add signature calc and description

---
+++ practical-02.1/src/aufgabe/Signatur.java
@@ -0,0 +1,37 @@
+package aufgabe;
+
+import java.math.BigInteger;
+
+public class Signatur {
+
+	public static void main(String[] args) {
+		BigInteger m_check;
+		BigInteger p = new BigInteger("61");
+		BigInteger q = new BigInteger("53");
+		BigInteger phi = new BigInteger("730");
+		BigInteger e = new BigInteger("17");
+		BigInteger n = p.multiply(q);
+		
+		BigInteger d = e.modInverse(phi);
+		
+		System.out.println("n: "+ n);
+		System.out.println("phi: "+ phi);
+		System.out.println("d: "+ d);
+		
+		BigInteger m = new BigInteger("5");
+		BigInteger signature_p1 = m.pow(d.intValue());
+		BigInteger signature = signature_p1.mod(n);
+		System.out.println(signature);
+		
+		m_check = signature.modPow(e, n);
+		System.out.println("m: "+m);
+		System.out.println("m-check: "+m_check);
+		
+		if(m_check.equals(m)){
+			System.out.println("Signatur stimmt.");
+		}
+		else {
+			System.out.println("Signatur stimmt nicht.");
+		}
+	}
+}
