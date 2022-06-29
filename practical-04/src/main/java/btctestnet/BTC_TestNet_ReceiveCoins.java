80eb9c0bb11202dc26f113e588f5346cc9637e70
fhildeb <felix@lukso.io>
2022-06-29 23:19:59 +0200

    cleanup

--- practical-04/src/main/java/btctestnet/BTC_TestNet_ReceiveCoins.java
+++ practical-04/src/main/java/btctestnet/BTC_TestNet_ReceiveCoins.java
@@ -11,19 +11,6 @@ import org.bitcoinj.wallet.Wallet;
 import org.bitcoinj.wallet.listeners.WalletCoinsReceivedEventListener;
 
 public class BTC_TestNet_ReceiveCoins {
-
-	//	Erster Output:	
-		//	Sende Geld an: mu7wJpNEoUspu8GdE8ngPSF8t9fLzbm4dU
-		//	Erhaltene Transaktion für 0.0001 BTC :   b281d0ea1d95aa94052ad8c6daf40eb413d778abdde26876012f4a851b7aca59
-		//	  updated: 2019-04-26T13:39:17Z
-		//	  version 2
-		//	  time locked until block 1512508
-		//	     in   PUSHDATA(22)[00141a7e8ceff06dc46ca76c7699cd47fc13be0fce61]
-		//	          outpoint:c494955f84f970838d214bd165f25fb26558e2aa6e8571b2b7116c688dc8c087:1
-		//	          sequence:fffffffe
-		//	     out  DUP HASH160 PUSHDATA(20)[95375ebdac677ff408d660d03f2684add2b753eb] EQUALVERIFY CHECKSIG 0.0001 BTC
-		//	     out  HASH160 PUSHDATA(20)[c5b850cd04aa5204eaa0cca328e8bff3ffe4a64f] EQUAL 0.01946742 BTC
-		//	     prps UNKNOWN
 	
 	private static WalletAppKit kit;
 	private static boolean gotCoins = false;
